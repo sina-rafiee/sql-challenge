b8a44765012438d4c5481c92fe20244ae6ac4a38
Sina <30na.rafiee@gmail.com>
2023-01-24 08:22:42 -0600

    EmployeeSQLfile.sql

--- EmployeeSQL/EmployeeSQLfile.sql
+++ EmployeeSQL/EmployeeSQLfile.sql
@@ -1,11 +1,63 @@
+
+CREATE TABLE titles (
+  title_id VARCHAR NOT NULL,
+  title VARCHAR NOT NULL,
+	PRIMARY KEY (title_id)
+);
+
+CREATE TABLE employees (
+  emp_no integer NOT NULL,
+  emp_title_id VARCHAR NOT NULL,
+  birth_date date NOT NULL,
+  first_name VARCHAR NOT NULL,
+  last_name VARCHAR NOT NULL,
+  sex VARCHAR NOT NULL,
+  hire_date date NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
+);
+
+CREATE TABLE departments (
+  dept_no VARCHAR NOT NULL,
+  dept_name VARCHAR NOT NULL,
+	PRIMARY KEY (dept_no)
+);
+
+CREATE TABLE dept_emp (
+  emp_no integer NOT NULL,
+  dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE dept_manager (
+  dept_no VARCHAR NOT NULL,
+  emp_no integer NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+
+CREATE TABLE salaries (
+  emp_no integer NOT NULL,
+  salary integer NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
+
+
+
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees
 INNER JOIN salaries
 ON employees.emp_no = salaries.emp_no;
 
+
 SELECT employees.last_name, employees.first_name, employees.hire_date
 FROM employees
 WHERE employees.hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+
 
 SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
 FROM ((departments
@@ -45,5 +97,50 @@ SELECT employees.last_name, COUNT(employees.last_name) AS LastNameCount
 FROM employees
 GROUP BY employees.last_name
 ORDER BY LastNameCount DESC;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
